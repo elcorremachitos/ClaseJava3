f4df6028e0c9fbc81acca9c7300a3ef6324e4f65
elcorremachitos <elcorremachitos.2015@gmail.com>
2025-08-12 10:40:58 -0500

    Ejercicio 1 corregido el Package (ahora si jaja)

--- src/Ejercicio1/mainAlquilerLavadora.java
+++ src/Ejercicio1/mainAlquilerLavadora.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 
 public class mainAlquilerLavadora {
     public static void main(String[] args) {
-        Ejercicio9.AlquilerLavadora lavadora = new Ejercicio9.AlquilerLavadora();
+        Ejercicio1.AlquilerLavadora lavadora = new Ejercicio1.AlquilerLavadora();
         lavadora.setHoras();
         lavadora.setTarifa();
         JOptionPane.showMessageDialog(null,"Subtotal: " + lavadora.calcularSubtotal() + " Descuento: " + lavadora.calcularDescuento() + " Totall: " + lavadora.calcularTotal());
